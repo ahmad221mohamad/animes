5cc1979f3ef4f5ae9a5c87ce45a711c1113b3139
ahmad221mohamad <94461500+ahmad221mohamad@users.noreply.github.com>
2023-01-11 07:25:47 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 pip install django
 pip install cloudinary
 pip install django-truncate
+pip install django-cors-headers
 pip install -r requirements.txt --upgrade
 python3.9 manage.py collectstatic
