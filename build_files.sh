5f811200407d6b7329662cc0c0512b4ad82d1180
ahmad221mohamad <94461500+ahmad221mohamad@users.noreply.github.com>
2023-01-11 07:49:26 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,7 @@ pip install cloudinary
 pip install django-truncate
 pip install django-cors-headers
 pip install django-rest-framework
-pip install sqlite3 
 pip install -r requirements.txt --upgrade
 python3.9 manage.py collectstatic
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
